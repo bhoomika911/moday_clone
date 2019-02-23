0ed436c26ea3190a7264b13787268a40bca219bd
Bhoomi <bhoomika911>
2019-02-23 17:04:37 +0530

    Work flow

--- src/Components/WorkFlowComponent/WorkFlow.js
+++ src/Components/WorkFlowComponent/WorkFlow.js
@@ -9,6 +9,33 @@ export default class WorkFlow extends Component {
     }
   }
 
+  renderTeam(){
+    return (
+      <div className='team-div extra-style'>
+        <div className='person-div-single' style={{zIndex : 10,left : 10}}>
+          <img src="./../../../dist/img/avatar.png"
+            className = 'team-img'
+            />
+          <div className = "person-online-status"/>
+        </div>
+
+        <div className='person-div-single' style={{zIndex : 9,left : 5}}>
+          <img src="./../../../dist/img/avatar2.png"
+            className = 'team-img'
+          />
+          <div className = "person-online-status"/>
+        </div>
+
+        <div className='person-div-single'  style={{zIndex : 8,left : 0}}>
+          <img src="./../../../dist/img/avatar3.png"
+            className = 'team-img'
+           />
+           <div className = "person-online-status"/>
+        </div>
+      </div>
+    )
+  }
+
   render(){
     return (
       <div className = "workflow-parent">
@@ -41,7 +68,118 @@ export default class WorkFlow extends Component {
             <div className = "fa fa-angle-down"/>
           </div>
         </div>
+        <div className = "workflow-container">
+          <div className = "workflow-container-project-det">
+            <div className = "workflow-container-project-name">PROJECT NAME</div>
+            <div className = "workflow-container-project-active-since">Active last 24 Hours</div>
+          </div>
+          <div className = "workflow-container-back-arrow-container">
+            <div className = "fa fa-angle-left fa-left-size"/>
+          </div>
+          <div className = "workflow-container-workflow-container">
+            <div className = "workflow-tabs-headers">
+
+              <div className = "">Research & Quality Lead</div>
+            </div>
+            <div className = "workflow-tabs-headers">
+              <div className = "">Discovery Call</div>
+            </div>
+            <div className = "workflow-tabs-headers">
+              <div className = "">Demo 1</div>
+            </div>
+            <div className = "workflow-tabs-headers">
+              <div className = "">Demo 2</div>
+            </div>
+            <div className = "workflow-tabs-headers">
+              <div className = "">Purpose</div>
+            </div>
+            <div className = "workflow-tabs-headers">
+              <div className = "">Close</div>
+            </div>
+            {
+              <div className = "workflow-container-workflow-indicator">
+                <div className = "workflow-tabs">
+                {
+                  this.renderTeam()
+                }
+                  <div className = "workflow-start-end-row-indicator"/>
+                  <div className = "workflow-status-indicator workflow-status-complition">
+                    <div className = "workflow-status-complition-circle">
+                    </div>
+                  </div>
+                  <div className = "workflow-row-indicator"/>
+                </div>
+                <div className = "workflow-tabs">
+                  <div className = "workflow-row-indicator"/>
+                  <div className = "workflow-status-indicator"/>
+                  <div className = "workflow-row-indicator"/>
+                </div>
+                <div className = "workflow-tabs">
+                  <div className = "workflow-row-indicator"/>
+                  <div className = "workflow-status-indicator"/>
+                  <div className = "workflow-row-indicator"/>
+                </div>
+                <div className = "workflow-tabs">
+                  <div className = "workflow-row-indicator"/>
+                  <div className = "workflow-status-indicator"/>
+                  <div className = "workflow-row-indicator"/>
+                </div>
+                <div className = "workflow-tabs">
+                  <div className = "workflow-row-indicator"/>
+                  <div className = "workflow-status-indicator"/>
+                  <div className = "workflow-row-indicator"/>
+                </div>
+                <div className = "workflow-tabs">
+                  <div className = "workflow-row-indicator"/>
+                  <div className = "workflow-status-indicator"/>
+                  <div className = "workflow-start-end-row-indicator"/>
+                </div>
+              </div>
+            }
+
+          </div>
+
+        </div>
       </div>
     )
   }
 }
+
+// <ul className="timeline" id="timeline">
+//   <li className="li complete">
+//     <div className="timestamp">
+//       <span className="author">Abhi Sharma</span>
+//       <span className="date">11/15/2014</span>
+//     </div>
+//     <div className="status">
+//       <h4> Shift Created </h4>
+//     </div>
+//   </li>
+//   <li className="li complete">
+//     <div className="timestamp">
+//       <span className="author">PAM Admin</span>
+//       <span className="date">11/15/2014</span>
+//     </div>
+//     <div className="status">
+//       <h4> Email Sent </h4>
+//     </div>
+//   </li>
+//   <li className="li complete">
+//     <div className="timestamp">
+//       <span className="author">Aaron Rodgers</span>
+//       <span className="date">11/15/2014</span>
+//     </div>
+//     <div className="status">
+//       <h4> SIC Approval </h4>
+//     </div>
+//   </li>
+//   <li className="li">
+//     <div className="timestamp">
+//       <span className="author">PAM Admin</span>
+//       <span className="date">TBD</span>
+//     </div>
+//     <div className="status">
+//       <h4> Shift Completed </h4>
+//     </div>
+//   </li>
+// </ul>
