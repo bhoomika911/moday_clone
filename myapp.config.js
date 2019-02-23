b4df1a52ed7895e750e4d71535c8fc64da33fae6
Bhoomi Kathiriya <v.bhoomi@virtualgodown.com>
2019-02-23 16:53:41 +0530

    changes

--- myapp.config.js
+++ myapp.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  {
-    name       : "myapp-client",
-    script     : "./src/index.js",
-    watch       : true,
-    env: {
-      "NODE_ENV": "development",
-    },
-    env_production : {
-      "NODE_ENV": "production"
-    }
+  apps : [
+    {
+      name       : "myapp-client",
+      script     : "./src/index.js",
+      watch       : true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production : {
+        "NODE_ENV": "production"
+      }
+    }]
   }]
 }
