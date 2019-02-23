56fad303e52344b4b5fc85b915c37d1b66003013
Bhoomi Kathiriya <v.bhoomi@virtualgodown.com>
2019-02-23 16:50:43 +0530

    deployment

---
+++ myapp.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  {
+    name       : "myapp-client",
+    script     : "./src/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+      "NODE_ENV": "production"
+    }
+  }]
+}
